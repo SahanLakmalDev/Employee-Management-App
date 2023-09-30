e14ba95c6e8eda58430ec0c6f11971c02419f5af
Sahan Lakmal <sahanloser@gmail.com>
2023-09-30 11:55:00 +0530

    Implement the UI

--- employee-management-app/src/main/java/lk/ijse/dep11/emp_app/controller/MainViewController.java
+++ employee-management-app/src/main/java/lk/ijse/dep11/emp_app/controller/MainViewController.java
@@ -1,4 +1,28 @@
 package lk.ijse.dep11.emp_app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MainViewController {
+    public AnchorPane root;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContact;
+    public Button btnSave;
+    public Button btnDelete;
+    public TableView tbvEmployees;
+    public TextField txtSearch;
+    public Button btnNew;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
 }
